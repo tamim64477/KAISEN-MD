5bff5df13c82688bf5dc8aad5d629bb862babc0c
After <arafatahammad667@gmail.com>
2025-07-22 15:52:15 +0600

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "KAISEN~6l4D1AiB#cxfN5sWPRxWkicPixa00qCuxQHlV0DnYNw4yxdjqGVw",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -25,9 +25,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "917003816486",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "01871974894",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "KAISEN",
+OWNER_NAME: process.env.OWNER_NAME || "arafat-kamran",
 // add bot owner name 
 ADMIN_STATUS: process.env.ADMIN_STATUS || "false",
 // make true to know who dismiss or promoted a member in group
